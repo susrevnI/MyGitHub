df9e870b627a09298f901f4bd1870312cde12eba
susrevnI <100325119+susrevnI@users.noreply.github.com>
2022-06-06 10:53:16 -0300

    Add files via upload

---
+++ Codigos Java/Aula 2022-05-31/src/br/edu/iftm/rpgjava/main/Escolhe.java
@@ -0,0 +1,30 @@
+/*package src.br.edu.iftm.rpgjava.main;
+
+import src.br.edu.iftm.rpgjava.classes.Arqueiro;
+import src.br.edu.iftm.rpgjava.classes.Guerreiro;
+import src.br.edu.iftm.rpgjava.classes.Mago;
+
+public class Escolhe {
+
+    Guerreiro g = new Guerreiro("", 30, 30, 30, 30,
+            30, 30, 30, 30, 30, 30);
+    Arqueiro a = new Arqueiro("", 30, 30, 30, 30,
+            30, 30, 30, 30, 30, 30);
+    Mago m = new Mago("", 30, 30, 30, 30,
+            30, 30, 30, 30, 30, 30);
+
+    String jogar1;
+
+    public String elcolher(char joga) {
+
+        if (joga == 'G') {
+            jogar1 = g.getNome();
+        } else if (joga == 'A'){
+            jogar1 = a.getNome();
+        } else if (joga == 'M'){
+            jogar1 = m.getNome();
+        }
+        return jogar1;
+    }
+}
+*/
