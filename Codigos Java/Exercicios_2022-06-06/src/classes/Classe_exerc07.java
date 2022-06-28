012aab88125f4ca4fc3d9792ba3f738f190d5d0a
susrevnI <100325119+susrevnI@users.noreply.github.com>
2022-06-28 13:56:48 -0300

    Correção exercicio 7

--- Codigos Java/Exercicios_2022-06-06/src/classes/Classe_exerc07.java
+++ Codigos Java/Exercicios_2022-06-06/src/classes/Classe_exerc07.java
@@ -3,7 +3,7 @@ package classes;
 public class Classe_exerc07 {
 
     private final int[] numeros = new int[10];
-    int i = 0;
+    private int i = 0;
 
     public void guardar(int num) {
         numeros[i] = num;
@@ -11,17 +11,18 @@ public class Classe_exerc07 {
     }
 
     public void calcular() {
-        int soma = 0;
-        int media = 0;
+        double soma = 0;
+        double media = 0;
         for (int i = 0; i < 10; i++) {
             if (numeros[i] % 3 == 0) {
                 soma += numeros[i];
                 media++;
             }
         }
+
         if (soma != 0) {
             media = soma / media;
         }
-        System.out.printf("A media dos multiplos de '3' foi '%d'.", media);
+        System.out.printf("A media dos multiplos de '3' foi '%f'.", media);
     }
 }
