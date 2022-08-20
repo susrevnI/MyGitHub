c8c175fdac7e9c2c33c522e74d4c161c98920de8
Pedro Henrique <pedrohtromao3000@gmail.com>
2022-08-20 14:23:56 -0300

    Atualizando o trabalho de Banco de Dados

--- Banco_de_Dados/Aula_2022-08-17/Teste.py
+++ Banco_de_Dados/Aula_2022-08-17/Teste.py
@@ -1,5 +1,12 @@
 espaco = input(":")
 
+lista1 = [("'Empregado',") ("'Produto',") ("'Compra',")]
+
+frase = "A frase é"
+
+for elemento in lista1:
+    frase += " " + elemento[0]
+
 # print(f"eis o{espaco.strip()}espaço")
 
 # while True:
@@ -13,6 +20,8 @@ espaco = input(":")
 # espaco = espaco.strip()
 
 # print(f"eis o{espaco}espaço")
+
+print(frase)
 
 if espaco == '1' or espaco == '0':
     print("Sim")
--- Banco_de_Dados/Aula_2022-08-17/TrabalhoBD.py
+++ Banco_de_Dados/Aula_2022-08-17/TrabalhoBD.py
@@ -53,10 +53,10 @@ def procurar(lista, elementoEsc, tipoElemento):
 
 
 # Função para imprimir as buscas
-def imprimirLista(myresult):
+def imprimirLista(lista):
     i = 1
 
-    for elemento in myresult:
+    for elemento in lista:
         print(f'{i} - {elemento[0]}')
         i += 1
 
@@ -123,50 +123,152 @@ imprimirLista(myresult)
 
 print()
 
-colunaEsc = input("Digite o nome ou número de uma coluna: ")
+
+# Perguntando se deseja mostrar colunas expecificas
+mostrarColuna = input("Deseja mostrar colunas especificas? (s/n): ")
+
+while mostrarColuna != 'S' and mostrarColuna != 's' and mostrarColuna != 'n' and mostrarColuna != 'N':
+    mostrarColuna = input("É necessario que digite 's' ou 'n': ")
+
+
+# Para mostrar colunas expecificas
+if mostrarColuna == 's' or mostrarColuna == 'S':
+    print("\nDigite 'Sair' para sair.")
+    listaColunas = list()
+    
+
+    qualColuna = input("\nDigite o nome ou numero da coluna: ")
+    qualColuna.strip()
+
+    while True:
+        if (qualColuna == "Sair" or qualColuna == "sair") and len(listaColunas) != 0:
+            break
+        elif (qualColuna == "Sair" or qualColuna == "sair") and len(listaColunas) == 0:
+            mostrarColuna = 'n'
+            break
+            
+        l = 1
+
+        for elemento in colunas:
+            if elemento[0] == qualColuna or qualColuna == str(l):
+                listaColunas.append(elemento[0])
+                sim = True
+                break
+            else:
+                sim = False
+            l += 1
+            
+        if not sim:
+            qualColuna = input("Coluna não encontrada, digite novamente: ")
+            qualColuna.strip()
+        else:
+            qualColuna = input("Digite o nome ou numero da proxima coluna: ")
+            qualColuna.strip()
+
+
+# Perguntando a coluna para realizar a busca
+colunaEsc = input("\nDigite o nome ou número de uma coluna para realizar uma busca: ")
 
 colunaEsc = colunaEsc.strip()
 
+continuarProg = True
+
+
+# Verificando se fará busca por coluna
 if colunaEsc != '':
     colunaEsc = procurar(myresult, colunaEsc, "Coluna")
 
+
+    # Se for realizar busca por coluna
     if colunaEsc != '':
+
+
+        # Tratando colunas expecificas escolhidas
+        if mostrarColuna == 's' or mostrarColuna == 'S':
+            select = "select "
+            k = len(listaColunas)
+            o = 1
+            for elemento in listaColunas:
+                if k != o:
+                    select += elemento + ", "
+                    o += 1
+                else:
+                    select += elemento
+
         
         # Realizando e armazenando a busca com base na coluna se for escolhida
-        print()
+        continuar = 's'
 
-        procura = input(f"Digite o '{colunaEsc}': ")
+        while True:
 
-        procura = procura.strip()
+            if continuarProg == False:
+                break
 
-        if procura != '':
-            mycursor.execute(f"select * from {tabelaEsc} where {colunaEsc} = '{procura}'")
-        else:
-            mycursor.execute(f"select * from {tabelaEsc}")
+            if continuar == 's' or continuar == 'S':
+                procura = input(f"\nDigite o/a '{colunaEsc}': ")
+                procura = procura.strip()
+        
 
+            # Caso tenha colunas expecificas
+            if (mostrarColuna == 's' or mostrarColuna == 'S') and procura != '':
+                mycursor.execute(f"{select} from {tabelaEsc} where {colunaEsc} = '{procura}'")
+            elif mostrarColuna == 's' or mostrarColuna == 'S':
+                mycursor.execute(f"{select} from {tabelaEsc}")
+
+            #caso não tenha colunas expecificas
+            elif procura != '':
+                mycursor.execute(f"select * from {tabelaEsc} where {colunaEsc} = '{procura}'")
+            else:
+                mycursor.execute(f"select * from {tabelaEsc}")
+
+            myresult = mycursor.fetchall()
+
+            if len(myresult) != 0:
+                break
+            else:
+
+                # Caso não tenha resultados e o usuário não queira mais fazer a busca por coluna
+                continuar = input("\nNenhum resultado encontrado. Deseja continuar a procura por coluna? (s/n): ")
+                while continuar != 'S' and continuar != 's' and continuar != 'n' and continuar != 'N':
+                    continuar = input("\nÉ necessario que digite 's' ou 'n': ")
+                if continuar == 'n' or continuar == 'N':
+                    continuarProg = input("\nDeseja continuar o programa? (s/n): ")
+                    while continuarProg != 'S' and continuarProg != 's' and continuarProg != 'n' and continuarProg != 'N':
+                        continuarProg = input("\n necessario que digite 's' ou 'n': ")
+                    if continuarProg == 'n' or continuarProg == 'N':
+                        continuarProg = False
+                        break
+                    procura = ''
+
+            
+
+
+if continuarProg:
+    # Caso não realize busca por coluna
+    if colunaEsc == '':
+        mycursor.execute(f"select * from {tabelaEsc}")
         myresult = mycursor.fetchall()
 
-if colunaEsc == '':
-    mycursor.execute(f"select * from {tabelaEsc}")
-    myresult = mycursor.fetchall()
+
+    # Imprimindo os resultados na tela
+    j = 1
+
+    if len(myresult) > 500:
+        input(f"\nFoi encontrado {len(myresult)} resultados. É resultado pra caral%@ vai com calma ai.")
+    elif len(myresult) > 50:
+        input(f"\nFoi encontrado {len(myresult)} resultados. São muitos resultados, talvez alguns resultados de sua pesquisa não apareçam na tela.")
+    elif len(myresult) == 0:
+        print("\nNenhum resultado foi encontrado.")
 
 
-# Imprimindo os resultados na tela
-j = 1
+    for elemento in myresult:
+        i = 0
+        print(f"\nLinha de resutados número: {j}")
+        for elemento2 in elemento:
+            print(f"  {colunas[i][0]} = {elemento2}")
+            i += 1
+        if j % 50 == 0:
+            input()
+        j += 1
 
-if len(myresult) > 500:
-    print(f"\nFoi encontrado {len(myresult)} resultados. É resultado pra caral%@ vai com calma ai.")
-elif len(myresult) > 50:
-    print(f"\nFoi encontrado {len(myresult)} resultados. São muitos resultados, talvez alguns resultados de sua pesquisa não apareçam na tela.")
-elif len(myresult) == 0:
-    print("\nNenhum resultado foi encontrado.")
-
-for elemento in myresult:
-    i = 0
-    print(f"\nLinha de resutados número: {j}")
-    for elemento2 in elemento:
-        print(f"  {colunas[i][0]} = {elemento2}")
-        i += 1
-    if j % 50 == 0:
-        input()
-    j += 1
+print("\n\n ------- Fim do Programa -------")
